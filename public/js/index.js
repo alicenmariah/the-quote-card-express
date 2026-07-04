52eaf032a6bc5711863167056b06b74705935c5b
Ally Taylor <alicenmariah@gmail.com>
2026-07-04 00:22:47 -0400

    updated unsplash info

--- public/js/index.js
+++ public/js/index.js
@@ -23,19 +23,33 @@ const quotes = [
 ]
 
 async function getRandomImage() {
-    const client_id = "YOUR_ACCESS_KEY";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
     } catch (error) {
         console.error(error);
     }
 }
+
+// async function getRandomImage() {
+//     const client_id = "YOUR_ACCESS_KEY";
+//     const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+//     try {
+//         const response = await fetch(endpoint);
+//         const returnedData = await response.json();
+//         const receivedPhotoUrl = returnedData.urls.regular;
+
+//         const imgDiv = document.querySelector(".background-img");
+//         imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
 
 
 getRandomImage();
